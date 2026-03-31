ae6322c8b314c11800033ef7c67145d9fa521365
paw2007 <azfa4807@gmail.com>
2026-03-31 17:54:37 +0700

    commit 2

--- Looping_0062.cpp
+++ Looping_0062.cpp
@@ -11,4 +11,16 @@ int main(){
     for( int i=0; i<=4; i++){
         cout<<"Teknologi Informasi UMY"<<endl;
     }
-  
+    cout<<endl;
+    //untuk mendapatkanb urutan angka acak yg berbeda
+    srand(time(0));
+
+    //Perulangan dengan WHILE
+    cout <<"PERULANGAN DENGAN WHILE"<<endl;
+    x = 1 + rand()%10;
+
+    while(x<=5){
+        cout<<"Bilangan acak: "<<x <<endl;
+        x = rand()%10;
+    }
+
